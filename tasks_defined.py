99a21d0026a25da99b00eaeaeaaa6225ea467f2f
RAraghavarora <raghavarora999@yahoo.in>
2023-06-06 21:59:11 +0530

    tasks

---
+++ tasks_defined.py
@@ -0,0 +1,40 @@
+tasks = [
+    'Open Curtains',
+    'Prepare food',
+    'Serve food',
+    'Make the bed',
+    'Water the plants',
+    'Collect dirty clothes',
+    'Load dirty clothes',
+    'Run the washing machine',
+    'Dry the clothes',
+    'Vacuum the house',
+    'Take out the trash',
+    'Collect the dirty dishes',
+    'Clean dirty dishes in the sink',
+    'Add dishes to dishwasher',
+    'Run the dishwasher',
+    'Collect dishes from dishwasher',
+    'Clean the windows',
+    'Serve cookies and warm milk',
+    'Close lights for all the rooms',
+    'Iron the clothes',
+    'Fold the clothes',
+    'Put the clothes in the closet',
+    'Clean the bathroom',
+    'Clean the kitchen counters',
+    'Make a cup of coffee',
+    'Put leftover food in fridge',
+    'Clean the fridge',
+    'Remove expired food from fridge',
+    'Throw away spoilt food',
+    'Dust the furniture',
+    'Mop the floors',
+    'Clean the kitchen appliances'
+]
+
+time = [
+    '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM',
+    '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM',
+    '7:00 PM', '8:00 PM', '9:00 PM', '10:00 PM'
+]
