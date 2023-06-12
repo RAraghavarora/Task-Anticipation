eb7f99f70762ae380ef9e725c58f4971c073a162
RAraghavarora <raghavarora999@yahoo.in>
2023-06-12 17:16:41 +0530

    Routine for a week (Keeping tasks common, changing time)

---
+++ routine_1.py
@@ -0,0 +1,137 @@
+tasks = ['Open Curtains', 'Prepare food', 'Serve food', 'Make the bed', 'Water the plants', 'Collect dirty clothes', 'Prepare food', 'Serve food', 'Load dirty clothes', 'Run the washing machine', 'Vacuum the house', 'Dry the clothes', 'Take out the trash', 'Prepare food', 'Serve food', 'Put leftover food in the fridge']
+
+routine_day_1 = {
+    '7:00 AM': 'Open Curtains',
+    '8:00 AM': 'Prepare food',
+    '9:00 AM': 'Serve food',
+    '10:00 AM': 'Make the bed',
+    '11:00 AM': 'Water the plants',
+    '12:00 PM': 'Collect dirty clothes',
+    '1:00 PM': 'Prepare food',
+    '2:00 PM': 'Serve food',
+    '3:00 PM': 'Load dirty clothes',
+    '4:00 PM': 'Run the washing machine',
+    '5:00 PM': 'Vacuum the house',
+    '6:00 PM': 'Dry the clothes',
+    '7:00 PM': 'Take out the trash',
+    '8:00 PM': 'Prepare food',
+    '9:00 PM': 'Serve food',
+    '10:00 PM': 'Put leftover food in the fridge'
+}
+
+routine_day_2 = {
+    '7:00 AM': 'Prepare food',
+    '8:00 AM': 'Serve food',
+    '9:00 AM': 'Open Curtains',
+    '10:00 AM': 'Make the bed',
+    '11:00 AM': 'Water the plants',
+    '12:00 PM': 'Prepare food',
+    '1:00 PM': 'Serve food',
+    '2:00 PM': 'Collect dirty clothes',
+    '3:00 PM': 'Load dirty clothes',
+    '4:00 PM': 'Run the washing machine',
+    '5:00 PM': 'Vacuum the house',
+    '6:00 PM': 'Dry the clothes',
+    '7:00 PM': 'Take out the trash',
+    '8:00 PM': 'Prepare food',
+    '9:00 PM': 'Serve food',
+    '10:00 PM': 'Put leftover food in the fridge'
+}
+# 3 tasks shifted in time (breakfast, lunch)
+
+routine_day_3 = {
+    '7:00 AM': 'Open Curtains',
+    '8:00 AM': 'Water the plants',
+    '9:00 AM': 'Prepare food',
+    '10:00 AM': 'Serve food',
+    '11:00 AM': 'Collect dirty clothes',
+    '12:00 PM': 'Load dirty clothes',
+    '1:00 PM': 'Run the washing machine',
+    '2:00 PM': 'Dry the clothes',
+    '3:00 PM': 'Prepare food',
+    '4:00 PM': 'Serve food',
+    '5:00 PM': 'Vacuum the house',
+    '6:00 PM': 'Take out the trash',
+    '7:00 PM': 'Prepare food',
+    '8:00 PM': 'Serve food',
+    '9:00 PM': 'Make the bed',
+    '10:00 PM': 'Put leftover food in the fridge',
+}
+
+routine_day_4 = {
+    '7:00 AM': 'Open Curtains',
+    '8:00 AM': 'Make the bed',
+    '9:00 AM': 'Water the plants',
+    '10:00 AM': 'Prepare food',
+    '11:00 AM': 'Serve food',
+    '12:00 PM': 'Vacuum the house',
+    '1:00 PM': 'Collect dirty clothes',
+    '2:00 PM': 'Load dirty clothes',
+    '3:00 PM': 'Run the washing machine',
+    '4:00 PM': 'Dry the clothes',
+    '5:00 PM': 'Prepare food',
+    '6:00 PM': 'Serve food',
+    '7:00 PM': 'Take out the trash',
+    '8:00 PM': 'Prepare food',
+    '9:00 PM': 'Serve food',
+    '10:00 PM': 'Put leftover food in the fridge'
+}
+# 2 tasks swapped (make the bed, water the plants)
+
+routine_day_5 = {
+    '7:00 AM': 'Open Curtains',
+    '8:00 AM': 'Prepare food',
+    '9:00 AM': 'Serve food',
+    '10:00 AM': 'Make the bed',
+    '11:00 AM': 'Water the plants',
+    '12:00 PM': 'Collect dirty clothes',
+    '1:00 PM': 'Prepare food',
+    '2:00 PM': 'Serve food',
+    '3:00 PM': 'Load dirty clothes',
+    '4:00 PM': 'Run the washing machine',
+    '5:00 PM': 'Dry the clothes',
+    '6:00 PM': 'Vacuum the house',
+    '7:00 PM': 'Take out the trash',
+    '8:00 PM': 'Prepare food',
+    '9:00 PM': 'Serve food',
+    '10:00 PM': 'Put leftover food in the fridge'
+}
+# 2 tasks swapped (dry the clothes, vacuum the house)
+
+routine_day_6 = { 
+    '7:00 AM': 'Open Curtains', 
+    '8:00 AM': 'Water the plants', 
+    '9:00 AM': 'Make the bed', 
+    '10:00 AM': 'Prepare food', 
+    '11:00 AM': 'Serve food', 
+    '12:00 PM': 'Vacuum the house', 
+    '1:00 PM': 'Collect dirty clothes', 
+    '2:00 PM': 'Prepare food', 
+    '3:00 PM': 'Serve food', 
+    '4:00 PM': 'Load dirty clothes', 
+    '5:00 PM': 'Run the washing machine', 
+    '6:00 PM': 'Dry the clothes', 
+    '7:00 PM': 'Take out the trash', 
+    '8:00 PM': 'Prepare food', 
+    '9:00 PM': 'Serve food', 
+    '10:00 PM': 'Put leftover food in the fridge' 
+}
+
+routine_day_7 = { 
+    '7:00 AM': 'Open Curtains', 
+    '8:00 AM': 'Prepare food', 
+    '9:00 AM': 'Serve food', 
+    '10:00 AM': 'Water the plants', 
+    '11:00 AM': 'Make the bed', 
+    '12:00 PM': 'Prepare food', 
+    '1:00 PM': 'Serve food', 
+    '2:00 PM': 'Vacuum the house', 
+    '3:00 PM': 'Collect dirty clothes', 
+    '4:00 PM': 'Load dirty clothes', 
+    '5:00 PM': 'Run the washing machine', 
+    '6:00 PM': 'Dry the clothes', 
+    '7:00 PM': 'Take out the trash', 
+    '8:00 PM': 'Prepare food', 
+    '9:00 PM': 'Serve food', 
+    '10:00 PM': 'Put leftover food in the fridge' 
+}
