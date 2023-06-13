605855c8824f22f495476d6340edbed3c8ec6d0e
RAraghavarora <raghavarora999@yahoo.in>
2023-06-13 19:52:05 +0530

    Llama-CPU working on 7B params

--- try_llama.py
+++ try_llama.py
@@ -26,28 +26,41 @@ torch.distributed.init_process_group("nccl")
 world_size = int(os.environ.get("WORLD_SIZE", -1))
 initialize_model_parallel(world_size)
 torch.manual_seed(1)
-local_rank = 1
+local_rank = 0
 
-ckpt_dir = '/scratch/raghav.arora/llama/13b/'
+ckpt_dir = '/scratch/raghav.arora/llama/7b_rag/'
 checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
 
 ckpt_path = checkpoints[local_rank]
 torch.cuda.empty_cache()
 device = 'cpu'
-checkpoint = torch.load(ckpt_path, map_location=device)
-checkpoint = {k: v.to(device) for k, v in checkpoint.items()}
 with open(Path(ckpt_dir) / "params.json", "r") as f:
     params = json.loads(f.read())
+checkpoint = torch.load(ckpt_path, map_location=device)
+checkpoint = {k: v.to(device) for k, v in checkpoint.items()}
 
-model_args: ModelArgs = ModelArgs(
-    max_seq_len=256, max_batch_size=16, **params
-)
 tokenizer_path = '/scratch/raghav.arora/llama/tokenizer.model'
 tokenizer = Tokenizer(model_path=tokenizer_path)
+model_args: ModelArgs = ModelArgs(
+    max_seq_len=512, max_batch_size=2, **params
+)
 model_args.vocab_size = tokenizer.n_words
-
 torch.set_default_tensor_type(torch.FloatTensor)
 model = Transformer(model_args).to(device)
 model.load_state_dict(checkpoint, strict=False)
+
 generator = LLaMA(model, tokenizer)
 print(f"Loaded in {time.time() - start_time:.2f} seconds")
+prompts = [
+    "I believe the meaning of life is",
+    "Simply put, the theory of relativity states that ",
+
+]
+
+results = generator.generate(
+    prompts, max_gen_len=1024, temperature=0.8, top_p=0.95
+)
+
+for result in results:
+    print(result)
+    print("\n==================================\n")
