2111d3e8f7011704e4b60ec24b856e6375600fdf
RAraghavarora <raghavarora999@yahoo.in>
2023-06-14 22:53:46 +0530

    generate prompts for the fixed time slots

--- get_combination.py
+++ get_combination.py
@@ -1,145 +1,135 @@
 import random
 
 tasks = ['Open Curtains', 'Prepare breakfast', 'Prepare lunch', 'Prepare dinner', 'Serve breakfast', 'Serve lunch', 'Serve dinner', 'Make the bed', 'Water the plants', 'Collect dirty clothes', 'Prepare food', 'Serve food', 'Load dirty clothes', 'Run the washing machine', 'Vacuum the house', 'Dry the clothes', 'Take out the trash', 'Prepare food', 'Serve food', 'Put leftover food in the fridge']
-
 routine_day_1 = {
-    '7:00 AM': 'Open Curtains',
-    '8:00 AM': 'Prepare breakfast',
-    '9:00 AM': 'Serve breakfast',
-    '10:00 AM': 'Make the bed',
-    '11:00 AM': 'Water the plants',
-    '12:00 PM': 'Collect dirty clothes',
-    '1:00 PM': 'Prepare lunch',
-    '2:00 PM': 'Serve lunch',
-    '3:00 PM': 'Load dirty clothes',
-    '4:00 PM': 'Run the washing machine',
-    '5:00 PM': 'Vacuum the house',
-    '6:00 PM': 'Dry the clothes',
-    '7:00 PM': 'Take out the trash',
-    '8:00 PM': 'Prepare dinner',
-    '9:00 PM': 'Serve dinner',
-    '10:00 PM': 'Put leftover food in the fridge'
+    "7:00 AM": "Make the bed",
+    "8:00 AM": "Clean the kitchen appliances",
+    "9:00 AM": "Vacuum the house",
+    "10:00 AM": "Water the plants",
+    "11:00 AM": "Load dirty clothes",
+    "12:00 PM": "Run the washing machine",
+    "1:00 PM": "Prepare lunch",
+    "2:00 PM": "Serve lunch",
+    "3:00 PM": "Dry the clothes",
+    "4:00 PM": "Iron the clothes",
+    "5:00 PM": "Fold the clothes",
+    "6:00 PM": "Clean the windows",
+    "7:00 PM": "Prepare dinner",
+    "8:00 PM": "Serve dinner",
+    "9:00 PM": "Put leftover food in fridge",
+    "10:00 PM": "Close lights for all the rooms"
 }
-
 routine_day_2 = {
-    '7:00 AM': 'Prepare breakfast',
-    '8:00 AM': 'Serve breakfast',
-    '9:00 AM': 'Open Curtains',
-    '10:00 AM': 'Make the bed',
-    '11:00 AM': 'Water the plants',
-    '12:00 PM': 'Prepare lunch',
-    '1:00 PM': 'Serve lunch',
-    '2:00 PM': 'Collect dirty clothes',
-    '3:00 PM': 'Load dirty clothes',
-    '4:00 PM': 'Run the washing machine',
-    '5:00 PM': 'Vacuum the house',
-    '6:00 PM': 'Dry the clothes',
-    '7:00 PM': 'Take out the trash',
-    '8:00 PM': 'Prepare dinner',
-    '9:00 PM': 'Serve dinner',
-    '10:00 PM': 'Put leftover food in the fridge'
+    "7:00 AM": "Make the bed",
+    "8:00 AM": "Prepare Breakfast",
+    "9:00 AM": "Serve Breakfast",
+    "10:00 AM": "Mop the floors",
+    "11:00 AM": "Load dirty clothes",
+    "12:00 PM": "Run the washing machine",
+    "1:00 PM": "Prepare lunch",
+    "2:00 PM": "Serve lunch",
+    "3:00 PM": "Clean the kitchen counters",
+    "4:00 PM": "Clean dirty dishes in the sink",
+    "5:00 PM": "Serve cookies and warm milk",
+    "6:00 PM": "Clean the windows",
+    "7:00 PM": "Prepare dinner",
+    "8:00 PM": "Serve dinner",
+    "9:00 PM": "Throw away spoilt food",
+    "10:00 PM": "Take out the trash"
 }
-
 routine_day_3 = {
-    '7:00 AM': 'Open Curtains',
-    '8:00 AM': 'Water the plants',
-    '9:00 AM': 'Prepare breakfast',
-    '10:00 AM': 'Serve breakfast',
-    '11:00 AM': 'Collect dirty clothes',
-    '12:00 PM': 'Load dirty clothes',
-    '1:00 PM': 'Run the washing machine',
-    '2:00 PM': 'Dry the clothes',
-    '3:00 PM': 'Prepare lunch',
-    '4:00 PM': 'Serve lunch',
-    '5:00 PM': 'Vacuum the house',
-    '6:00 PM': 'Take out the trash',
-    '7:00 PM': 'Prepare dinner',
-    '8:00 PM': 'Serve dinner',
-    '9:00 PM': 'Make the bed',
-    '10:00 PM': 'Put leftover food in the fridge',
+    "7:00 AM": "Open Curtains",
+    "8:00 AM": "Clean the kitchen appliances",
+    "9:00 AM": "Dust the furniture",
+    "10:00 AM": "Water the plants",
+    "11:00 AM": "Load dirty clothes",
+    "12:00 PM": "Run the washing machine",
+    "1:00 PM": "Prepare lunch",
+    "2:00 PM": "Serve lunch",
+    "3:00 PM": "Clean the kitchen counters",
+    "4:00 PM": "Clean dirty dishes in the sink",
+    "5:00 PM": "Run the dishwasher",
+    "6:00 PM": "Clean the windows",
+    "7:00 PM": "Prepare dinner",
+    "8:00 PM": "Serve dinner",
+    "9:00 PM": "Collect dishes from dishwasher",
+    "10:00 PM": "Remove expired food from fridge"
 }
-
 routine_day_4 = {
-    '7:00 AM': 'Open Curtains',
-    '8:00 AM': 'Make the bed',
-    '9:00 AM': 'Water the plants',
-    '10:00 AM': 'Prepare breakfast',
-    '11:00 AM': 'Serve breakfast',
-    '12:00 PM': 'Vacuum the house',
-    '1:00 PM': 'Collect dirty clothes',
-    '2:00 PM': 'Load dirty clothes',
-    '3:00 PM': 'Prepare lunch',
-    '4:00 PM': 'Serve lunch',
-    '5:00 PM': 'Run the washing machine',
-    '6:00 PM': 'Dry the clothes',
-    '7:00 PM': 'Take out the trash',
-    '8:00 PM': 'Prepare dinner',
-    '9:00 PM': 'Serve dinner',
-    '10:00 PM': 'Put leftover food in the fridge'
+    "7:00 AM": "Make the bed",
+    "8:00 AM": "Prepare Breakfast",
+    "9:00 AM": "Serve Breakfast",
+    "10:00 AM": "Mop the floors",
+    "11:00 AM": "Load ",
+    "12:00 PM": "Run the washing machine",
+    "1:00 PM": "Prepare lunch",
+    "2:00 PM": "Serve lunch",
+    "3:00 PM": "Clean the kitchen counters",
+    "4:00 PM": "Add dishes to dishwasher",
+    "5:00 PM": "Run the dishwasher",
+    "6:00 PM": "Clean the windows",
+    "7:00 PM": "Prepare dinner",
+    "8:00 PM": "Serve dinner",
+    "9:00 PM": "Put leftover food in fridge",
+    "10:00 PM": "Close lights for all the rooms"
 }
-
 routine_day_5 = {
-    '7:00 AM': 'Open Curtains',
-    '8:00 AM': 'Prepare breakfast',
-    '9:00 AM': 'Serve breakfast',
-    '10:00 AM': 'Make the bed',
-    '11:00 AM': 'Water the plants',
-    '12:00 PM': 'Collect dirty clothes',
-    '1:00 PM': 'Prepare lunch',
-    '2:00 PM': 'Serve lunch',
-    '3:00 PM': 'Load dirty clothes',
-    '4:00 PM': 'Run the washing machine',
-    '5:00 PM': 'Dry the clothes',
-    '6:00 PM': 'Vacuum the house',
-    '7:00 PM': 'Take out the trash',
-    '8:00 PM': 'Prepare dinner',
-    '9:00 PM': 'Serve dinner',
-    '10:00 PM': 'Put leftover food in the fridge'
+    "7:00 AM": "Make the bed",
+    "8:00 AM": "Clean the kitchen appliances",
+    "9:00 AM": "Dust the furniture",
+    "10:00 AM": "Mop the floors",
+    "11:00 AM": "Load ",
+    "12:00 PM": "Run the washing machine",
+    "1:00 PM": "Prepare lunch",
+    "2:00 PM": "Serve lunch",
+    "3:00 PM": "Dry the clothes",
+    "4:00 PM": "Clean dirty dishes in the sink",
+    "5:00 PM": "Serve cookies and warm milk",
+    "6:00 PM": "Clean the windows",
+    "7:00 PM": "Prepare dinner",
+    "8:00 PM": "Serve dinner",
+    "9:00 PM": "Put leftover food in fridge",
+    "10:00 PM": "Close lights for all the rooms"
 }
-
-routine_day_6 = { 
-    '7:00 AM': 'Open Curtains', 
-    '8:00 AM': 'Water the plants', 
-    '9:00 AM': 'Make the bed', 
-    '10:00 AM': 'Prepare breakfast', 
-    '11:00 AM': 'Serve breakfast', 
-    '12:00 PM': 'Vacuum the house', 
-    '1:00 PM': 'Collect dirty clothes', 
-    '2:00 PM': 'Prepare lunch', 
-    '3:00 PM': 'Serve lunch', 
-    '4:00 PM': 'Load dirty clothes', 
-    '5:00 PM': 'Run the washing machine', 
-    '6:00 PM': 'Dry the clothes', 
-    '7:00 PM': 'Take out the trash', 
-    '8:00 PM': 'Prepare dinner', 
-    '9:00 PM': 'Serve dinner', 
-    '10:00 PM': 'Put leftover food in the fridge' 
+routine_day_6 = {
+    "7:00 AM": "Make a cup of coffee",
+    "8:00 AM": "Clean the kitchen appliances",
+    "9:00 AM": "Vacuum the house",
+    "10:00 AM": "Mop the floors",
+    "11:00 AM": "Load dirty clothes",
+    "12:00 PM": "Run the washing machine",
+    "1:00 PM": "Prepare lunch",
+    "2:00 PM": "Serve lunch",
+    "3:00 PM": "Dry the clothes",
+    "4:00 PM": "Add dishes to dishwasher",
+    "5:00 PM": "Run the dishwasher",
+    "6:00 PM": "Clean the windows",
+    "7:00 PM": "Prepare dinner",
+    "8:00 PM": "Serve dinner",
+    "9:00 PM": "Collect dishes from dishwasher",
+    "10:00 PM": "Remove expired food from fridge"
 }
-
-routine_day_7 = { 
-    '7:00 AM': 'Open Curtains', 
-    '8:00 AM': 'Prepare breakfast', 
-    '9:00 AM': 'Serve breakfast', 
-    '10:00 AM': 'Water the plants', 
-    '11:00 AM': 'Make the bed', 
-    '12:00 PM': 'Prepare lunch', 
-    '1:00 PM': 'Serve lunch', 
-    '2:00 PM': 'Vacuum the house', 
-    '3:00 PM': 'Collect dirty clothes', 
-    '4:00 PM': 'Load dirty clothes', 
-    '5:00 PM': 'Run the washing machine', 
-    '6:00 PM': 'Dry the clothes', 
-    '7:00 PM': 'Take out the trash', 
-    '8:00 PM': 'Prepare dinner', 
-    '9:00 PM': 'Serve dinner', 
-    '10:00 PM': 'Put leftover food in the fridge' 
+routine_day_7 = {
+    "7:00 AM": "Clean the bathroom",
+    "8:00 AM": "Clean the kitchen appliances",
+    "9:00 AM": "Dust the furniture",
+    "10:00 AM": "Collect dirty clothes",
+    "11:00 AM": "Load dirty clothes",
+    "12:00 PM": "Run the washing machine",
+    "1:00 PM": "Prepare lunch",
+    "2:00 PM": "Serve lunch",
+    "3:00 PM": "Clean the kitchen counters",
+    "4:00 PM": "Clean dirty dishes in the sink",
+    "5:00 PM": "Serve cookies and warm milk",
+    "6:00 PM": "Clean the windows",
+    "7:00 PM": "Prepare dinner",
+    "8:00 PM": "Serve dinner",
+    "9:00 PM": "Put leftover food in fridge",
+    "10:00 PM": "Take out the trash"
 }
-
 
 prompt = '''
 # Based on following routine, predict a possible task sequence for day 7
-
-tasks = ['Open Curtains', 'Prepare breakfast', 'Prepare lunch', 'Prepare dinner', 'Serve breakfast', 'Serve lunch', 'Serve dinner', 'Make the bed', 'Water the plants', 'Collect dirty clothes', 'Load dirty clothes', 'Run the washing machine', 'Vacuum the house', 'Dry the clothes', 'Take out the trash', 'Put leftover food in the fridge', 'Throw discarded food in the trash']
 
 routine_day_1 = %s
 
@@ -155,6 +145,7 @@ routine_day_6 = %s
 
 # Anticipate the output of routine_day_7
 
+```
 routine_day_7 = {
     '7:00 AM': '',
     '8:00 AM': '',
@@ -173,11 +164,11 @@ routine_day_7 = {
     '9:00 PM': '',
     '10:00 PM': ''
 }
-
+```
 '''
 
 for _ in range(100):
-    output_file = open('gpt_prompts/%d.txt'%(_), 'w')
+    output_file = open('gpt_prompts/fixed_time/%d.txt'%(_), 'w')
     routines = [routine_day_1, routine_day_2, routine_day_3, routine_day_4, routine_day_5, routine_day_6, routine_day_7]
     selected_routines = random.sample(routines, 6)
 
