2ee30d1f8c17bc8c7a1a10418ecfce7bd77ce0c9
RAraghavarora <raghavarora999@yahoo.in>
2023-07-06 15:47:45 +0530

    Bounding boxes in AI2THOR scene

---
+++ ai2thor_bbox.py
@@ -0,0 +1,56 @@
+from PIL import Image, ImageDraw
+from ai2thor.controller import Controller
+import ai2thor
+print(ai2thor.__version__)
+from ai2thor.platform import CloudRendering
+from ai2thor.video_controller import VideoController
+import cv2
+import numpy as np
+
+controller = Controller(platform=CloudRendering, renderInstanceSegmentation=True, scene='FloorPlan1', width=720, height=720, fieldOfView=90)
+event = controller.step(
+    action="GetReachablePositions"
+)
+frames = []
+for _ in range(90//5):
+    frames.append(controller.step(action="RotateLeft", degrees=5).frame)
+
+event = controller.step(
+    action='SetObjectStates', 
+    SetObjectStates={
+            'objectType': 'Bottle',
+            'stateChange': 'canFillWithLiquid', 
+            'isFilledWithLiquid': True,
+        }
+)
+
+mask = np.array(event.instance_masks.mask('HousePlant|-01.95|+00.89|-02.52'))
+detec = event.instance_detections2D
+frame = event.frame
+
+# Convert the instance segmentation frame to an image
+segmentation_img = Image.fromarray(frame.astype(np.uint8))
+
+# Create a new image with the bounding boxes
+bbox_img = segmentation_img.copy()
+draw = ImageDraw.Draw(bbox_img)
+
+# Iterate over the detections and draw bounding boxes
+obj_ids = [
+    'Stove|0|0|0',
+    'Window|-0.54|2.595|-1.5',
+    'CoffeeMachine|-01.98|+00.90|-00.19',
+    'Microwave|-00.24|+01.69|-02.53',
+    'Apple|-00.47|+01.15|+00.48',
+    'Tomato|-00.39|+01.14|-00.81',
+    'HousePlant|-01.95|+00.89|-02.52',
+]
+for obj_id in obj_ids:
+    detection = detec[obj_id]
+    x1, y1, x2, y2 = detection
+    label = obj_id
+    draw.rectangle([(x1, y1), (x2, y2)], outline='red')
+    draw.text((x1, y1-10), label, fill='green')
+
+# Save the image with bounding boxes
+bbox_img.save('bounding_boxes.png')
